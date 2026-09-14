aa01536ec165d115474ab1fe1c7d54707a9b80cf
BinaryBunny345 <cora.davis@ucas-edu.net>
2026-09-14 14:53:05 -0600

    memory diagram

---
+++ memory_diagram_activity.py
@@ -0,0 +1,28 @@
+# CD, Memory Diagram Activity 
+
+name = "Alex"
+print(f"The variable Alex has a value of {"Alex"} and is saved at {id(name)}")
+
+age = 14
+print(f"The variable 14 has a value of {"14"} and is saved at {id(age)}")
+
+score = 10
+print(f"The variable 10 has a value of {"10"} and is saved at {id(score)}")
+
+score = 25
+print(f"The variable 25 has a value of {"25"} and is saved at {id(score)}")
+
+height = 5.9
+print(f"The variable 5.9 has a value of {"5.9"} and is saved at {id(height)}")
+
+city = "Denver"
+print(f"The variable Denver has a value of {"Denver"} and is saved at {id(city)}")
+
+zip_code = 80202
+print(f"The variable 80202 has a value of {"80202"} and is saved at {id(zip_code)}")
+
+price1 = 10
+print(f"The variable 10 has a value of {"10"} and is saved at {id(price1)}")
+
+price2 = 10
+print(f"The variable 10 has a value of {"10"} and is saved at {id(price2)}")
