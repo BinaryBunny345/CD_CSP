e384324f866d22cfa420b9fb1de2864f99c92631
BinaryBunny345 <cora.davis@ucas-edu.net>
2026-09-15 14:41:37 -0600

    hello user

---
+++ hello_user.py
@@ -0,0 +1,10 @@
+# CD, hello user
+
+while True:
+    name = input("What is your name?: ").strip().capitalize()
+    if name.isnumeric():
+        print("That is a number not a name!")
+    else:
+        break
+
+print(f"Hello, {name} what are you doing today?!")
