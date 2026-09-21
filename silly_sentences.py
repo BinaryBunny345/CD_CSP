48633bc3607ba0b77c2aa3f765dfc73a79a924ed
BinaryBunny345 <cora.davis@ucas-edu.net>
2026-09-21 14:32:37 -0600

    silly sentences

---
+++ silly_sentences.py
@@ -0,0 +1,10 @@
+# CD, Silly Sentences Activity
+
+verb_ing = input("Give me a verb ending in ing: ").strip().lower()
+place = input("Give me a place: ").strip().lower()
+color = input("Give me a color: ").strip().lower()
+travel = input("Give me a type of vehicle: ").strip().lower()
+emotion = input("Give me an emotion: ").strip().lower()
+animal = input("Give me an animal: ").strip().lower()
+
+print("After many days "+verb_ing+" in "+place+" I started to see "+color+" flood my vision. I hailed a "+travel+" with a very "+emotion+" driver and a "+animal+" on his lap.")
