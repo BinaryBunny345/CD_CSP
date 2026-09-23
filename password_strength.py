53119085bd8042eb61bbc4bab53aed651a0a0078
BinaryBunny345 <cora.davis@ucas-edu.net>
2026-09-23 14:53:16 -0600

    password strength

---
+++ password_strength.py
@@ -0,0 +1,24 @@
+# CD, password strength checker assignment
+#Ask user for password
+#Check if password is long enough
+#update length variable to True
+#For letter in password:
+    #if letter.isupper():
+        #update uppercse variable to True
+#.isslower()
+#.isnumeric
+#if letter in "!?@#$[]<>:"
+
+characters = False
+uppercase = False
+lowercase = False
+number =  False
+symbol = False
+password = input("What is your password? ")
+
+(len(password))
+for letter in password:
+    if (password) >= 8:
+        password = True
+    if (password) < 8:
+        password = False
