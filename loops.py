f36524d9e8d3ac551941dd59b195ad626ce7f1d0
BinaryBunny345 <cora.davis@ucas-edu.net>
2026-09-25 14:53:31 -0600

    number guessing game

--- loops.py
+++ loops.py
@@ -20,3 +20,26 @@ print("GOOSE!!!!!")
 # Complex Data Type = Holds other data in it
 family = ["Cooper", "Whitaker", "Jennifer", "Jared"]
 print(family[3])
+#Adding to a list
+family.append("Viola") # <= adds the item to the end of the list
+family.insert(2, "Cooper")
+print(family)
+# Remove item from a list
+family.pop() # <= if no number given pop removes the last item
+print(family)
+
+
+#print each item in list
+for family in families:
+    print(family)
+
+# For loops
+for num in range(1,25):
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
---
+++ number_guessing_game.py
@@ -0,0 +1,10 @@
+# CD, number guessing game assignment
+
+import random
+
+print("Hello, welcome to Cora's guessing game. You have six tries to guess the correct number.....READY SET GO!")
+guess1 = input("What is your first guess?: ")
+
+print(guess1)
+
+count = 1
